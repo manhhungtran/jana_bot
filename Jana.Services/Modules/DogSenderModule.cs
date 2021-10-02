b4e43e94ebe9676686c7fee76cee5163ae65237f
Tran Manh Hung <tran@webscope.io>
2021-10-03 00:37:20 +0200

    add basic command

---
+++ Jana.Services/Modules/DogSenderModule.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jana.Services.Modules
+{
+    public class DogSenderModule : ModuleBase
+    {
+        [Command("dog pls")]
+        public async Task SendDogAsync()
+        {
+            await ReplyAsync("Soon there will be doggo pictures ayyy :)");
+        }
+    }
+}
---
+++ Jana.Services/Modules/ModuleBase.cs
@@ -0,0 +1,21 @@
+﻿using Discord;
+using Discord.Commands;
+using Jana.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jana.Services.Modules
+{
+    public class ModuleBase : ModuleBase<SocketCommandContext>
+    {
+        //public static readonly Emoji TagNotFound = EmojiExtensions.FromText("mag_right");
+        //public static readonly Emoji Pass = EmojiExtensions.FromText("ok_hand");
+        //public static readonly Emoji Fail = EmojiExtensions.FromText("octagonal_sign");
+        //public static readonly Emoji Removed = EmojiExtensions.FromText("put_litter_in_its_place");
+
+        public JanaContext Database { get; set; }
+    }
+}
